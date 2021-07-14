898d74c83e10fa7a1a185ed3c7e63d75ac025d6f
bangkitwira <bangkitwira@gmail.com>
2021-07-14 21:53:09 +0700

    change some

--- app.js
+++ app.js
@@ -42,7 +42,14 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err)
 })
 
-app.listen(3000, () => {
-    console.log(api)
-    console.log('server is running  http://localhost:3000')
+// development
+// app.listen(3000, () => {
+//     console.log(api)
+//     console.log('server is running  http://localhost:3000')
+// })
+
+// production
+var server= app.listen(process.env.PORT || 3000, function() {
+    var port = server.address().port()
+    console.log("Express is working on port " + port)
 })
